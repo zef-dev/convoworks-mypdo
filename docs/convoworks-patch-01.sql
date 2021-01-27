42f24d45a99808c250162d66fc7a36fc1cc6b8d9
mmarcec007 <mark.marcec@gmail.com>
2021-01-27 11:27:07 +0100

    add NOT NULL in convoworks-patch-01.sql

--- docs/convoworks-patch-01.sql
+++ docs/convoworks-patch-01.sql
@@ -2,4 +2,4 @@
 -- Alter service_releases table by adding meta column to service_releases table
 -- ----------------------------------------------------------------------------------
 ALTER TABLE `service_releases`
-ADD COLUMN `meta` LONGTEXT AFTER `alias`;
+    ADD COLUMN `meta` LONGTEXT NOT NULL AFTER `alias`;
